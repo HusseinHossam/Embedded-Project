1ce16912a9122b156fe11c68e2b091941f9c801d
abdallah25025 <43056557+abdallah25025@users.noreply.github.com>
2018-10-01 03:27:07 +0200

    Update USART.h

--- USART.h
+++ USART.h
@@ -6,7 +6,7 @@
 #include "std_types.h"
 #include "common_macros.h"
 #include "micro_config.h"
-void USART_vdInit(uint_16 Baud_rate);
+void USART_vdInit(uint_32 Baud_rate);
 void USART_vdTransmit (uint_8 data);
 uint_8 USART_u8Recieve();
 void USART_vdSendString(uint_8* Str);
