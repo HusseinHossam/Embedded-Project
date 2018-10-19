316a230ef96da2bff4f2e9dbe6aeff6bd7e5964d
abdallah25025 <43056557+abdallah25025@users.noreply.github.com>
2018-10-19 02:39:56 +0200

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,10 @@
+#ifndef Timer_H_
+#define Timer_H_
+#include "std_types.h"
+#include "common_macros.h"
+#include "micro_config.h"
+
+void Timer_vdInit(uint_8 num , uint_8 mode, uint_8 value )
+uint_32 Timer2_u32count(void);
+void Timer_vdOFF(uint_8 Timer_name)
+#endif 
