e7fff3668e3f3211a6877c8615362520ae885d01
abdallah25025 <43056557+abdallah25025@users.noreply.github.com>
2018-11-19 22:47:17 +0200

    Create Timers_init.h

---
+++ Timers_init.h
@@ -0,0 +1,17 @@
+void ICU_Timer1_vdInit (void);
+void ICU_Timer1_vdStop (void);
+void ICU_Timer1_vdInit_Risingedge(void);
+void ICU_Timer1_vdInit_Fallingedge(void);
+void ICU_Timer1_vdWaitForRisingedge(void);
+void ICU_Timer1_vdWaitForFallingedge(void);
+
+void INT0_vdInit (void);
+void Timer2_vdInit (void);
+void Timer2_vdStop (void);
+
+void ICU_Timer3_vdInit (void);
+void ICU_Timer3_vdStop (void);
+void ICU_Timer3_vdInit_Risingedge(void);
+void ICU_Timer3_vdInit_Fallingedge(void);
+void ICU_Timer3_vdWaitForRisingedge(void);
+void ICU_Timer3_vdWaitForFallingedge(void);
